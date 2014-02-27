3c64026f2842022be05a05ca4b8e6f7d348358a7
gokhankcmn@gmail.com <gokhankcmn@gmail.com@dd0db48c-9bc1-24b3-bd37-a7e2cf38169d>
2014-02-27 07:34:50 +0000

    ba

---
+++ trunk/consistent-hash-sample/src/com/gokman/cloud/consistenthash/ConsistentHash.java
@@ -0,0 +1,53 @@
+package com.gokman.cloud.consistenthash;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class ConsistentHash<T> {
+
+	private final int numberOfReplica;
+	private final SortedMap<Integer,T> circle=new TreeMap<Integer,T>();
+	
+	public ConsistentHash(int numberOfReplica,Collection<T> nodes) throws NoSuchAlgorithmException{
+		this.numberOfReplica=numberOfReplica;
+		
+		for (T node : nodes){
+			add(node);
+		}
+	}
+	
+	private void add(T node) throws NoSuchAlgorithmException {
+		for(int i=0;i<numberOfReplica;i++){
+			circle.put(hash(node.toString()+1), node);
+		}
+	}
+	
+	public void remove(T node) throws NoSuchAlgorithmException{
+		for(int i=0;i<numberOfReplica;i++){
+			circle.remove(hash(node.toString()+1));
+		}
+	}
+	
+	public T get(String key) throws NoSuchAlgorithmException{
+		if(circle.isEmpty()){
+			return null;
+		}
+		int hash = hash(key);
+		if(!circle.containsKey(hash)){
+			SortedMap<Integer, T> tailMap=circle.tailMap(hash);
+			hash=tailMap.isEmpty()?circle.firstKey():tailMap.firstKey();
+		}
+		return circle.get(hash);
+	}
+
+	public int hash(String text) throws NoSuchAlgorithmException{
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+		messageDigest.update(text.getBytes());
+		
+		return Integer.parseInt(messageDigest.digest().toString());
+		
+	}
+}
---
+++ trunk/consistent-hash-sample/src/com/gokman/cloud/consistenthash/HashFunctionDene.java
@@ -0,0 +1,12 @@
+package com.gokman.cloud.consistenthash;
+
+import java.security.NoSuchAlgorithmException;
+
+public class HashFunctionDene {
+public static void main(String args[]) throws NoSuchAlgorithmException{
+	
+   int a=5;
+   int b= (a>4)? 10 : 20 ;
+   System.out.println(b);
+}
+}
